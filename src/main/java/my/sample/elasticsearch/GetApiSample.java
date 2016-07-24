82f180c4506167ff8d43bbdf2ae5fa8e09e5a1f9
Takahide FUJII <bvu80603@gmail.com>
2016-07-25 00:45:26 +0900

    update Get API Sample

--- src/main/java/my/sample/elasticsearch/GetApiSample.java
+++ src/main/java/my/sample/elasticsearch/GetApiSample.java
@@ -1,7 +1,10 @@
 package my.sample.elasticsearch;
 
 import my.sample.elasticsearch.util.EsUtil;
+import my.sample.elasticsearch.util.JsonGenerator;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 
 public class GetApiSample {
@@ -9,9 +12,28 @@ public class GetApiSample {
 
         try (Client client = EsUtil.clientBuilder()) {
 
+            // 準備
+            IndexRequest indexRequest1 = new IndexRequest("sample", "parent", "parent-uuid-1");
+            indexRequest1.source(JsonGenerator.generateNestedJsonArray());
+            client.index(indexRequest1);
+
+            IndexRequest indexRequest2 = new IndexRequest("twitter", "tweet", "1");
+            indexRequest2.source(JsonGenerator.generateJsonArray());
+            client.index(indexRequest2);
+
+
+            // index の作成が完了するのを待つ
+            Thread.sleep(1000);
+
             // index, type, document の id を指定
             GetResponse getResponse = client.prepareGet("sample", "parent", "parent-uuid-1").get();
             EsUtil.printGetResponse(getResponse);
+
+            // GetRequest を利用
+            GetRequest getRequest = new GetRequest("twitter", "tweet", "1");
+            getResponse = client.get(getRequest).get();
+            EsUtil.printGetResponse(getResponse);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
